de8198256ee028255bada4619cc6574c4bc3e38c
drsimonj <drsimonjackson@gmail.com>
2016-09-16 09:47:52 +1000

    Added q_read(); changed q_readh to q_readv

--- R/read.R
+++ R/read.R
@@ -1,28 +1,42 @@
-#' Read header rows of a qualtrics file into a tibble of variable names and
-#' labels
+#' Read variable information from a qualtrics file into a tibble of variable
+#' names and labels
 #'
 #' Reads in the first two lines of a Qualtrics Survey file. The first line
 #' contains the variable names and the second contains the complete labels, such
 #' as question text. The minor exception are the default Qualtrics variables,
 #' which are named V1, V2, and so on. The variable name for these are stored in
 #' the second row. Thus, for these, the label is substituted as the variable
-#' name. Any empty variables are also dropped (as files end with comma, leaving
-#' empty cell at end).
+#' name.
 #'
 #' @inheritParams readr::read_lines
 #' @export
 #'
 #' @return tibble with variable (`var`) and corresponding label columns.
-q_readh <- function(file) {
+q_readv <- function(file) {
   # Read the first two lines of the file
-  headers <- readr::read_lines(file, n_max = 2) %>%
+  .v <- readr::read_lines(file, n_max = 2) %>%
                stringr::str_split(",")
 
   # Replace that Qualtrics variables named V1, V2, etc...
-  to_replace <- stringr::str_detect(headers[[1]], "^V[0-9]+$")
-  headers[[1]][to_replace] <- headers[[2]][to_replace]
+  to_replace <- stringr::str_detect(.v[[1]], "^V[0-9]+$")
+  .v[[1]][to_replace] <- .v[[2]][to_replace]
 
-  names(headers) <- c("var", "label")
+  names(.v) <- c("var", "label")
 
-  tibble::as_tibble(headers) %>% dplyr::filter(var != "")
+  tibble::as_tibble(.v)
+}
+
+#' Read qualtrics file as a tibble
+#'
+#' @export
+q_read <- function(file) {
+  # Read vars
+  .v <- q_readv(file)
+
+  # Import data
+  .d <- readr::read_csv(file, skip = 2, col_names = .v$var)
+
+  # Return with `q_df` class
+  class(.d) <- c("q_df", class(.d))
+  .d
 }
